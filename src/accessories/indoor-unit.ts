homebridge-panasonic-ac-platform/homebridge-panasonic-ac-platform

commit 811fc16af2b7e5e3130e2e7eab7cb4ae827cb03a

Improve login error handling (#58)
- Introduce a maximum number of login retry attempts.
- Remove per-device login retries on the back of device status updates.
- Add troubleshooting instructions for login errors (see README).
- Increase app version number.

src/comfort-cloud.ts
@@ -3,7 +3,6 @@ import axios, { AxiosError } from 'axios';
 import {
   APP_VERSION,
   COMFORT_CLOUD_USER_AGENT,
-  LOGIN_RETRY_DELAY,
   LOGIN_TOKEN_REFRESH_INTERVAL,
 } from './settings';
 import {
@@ -22,7 +21,6 @@ import {
 export default class ComfortCloudApi {
   private token: string;
   private _loginRefreshInterval: NodeJS.Timer | undefined;
-  private _loginRetryTimeouts: NodeJS.Timer[] = [];
 
   constructor(
     private readonly config: PanasonicPlatformConfig,
@@ -38,15 +36,6 @@ export default class ComfortCloudApi {
   async login() {
     this.log.debug('Comfort Cloud: login()');
 
-    /**
-     * A repeat-login might have been requested by several accessories
-     * at a similar time. The first timeout to be executed can clear
-     * all remaining ones, since it doesn't make sense to log in multiple
-     * times within a short amount of time.
-     */
-    for (const timeoutId of this._loginRetryTimeouts) {
-      clearTimeout(timeoutId);
-    }
     clearInterval(<NodeJS.Timer>this._loginRefreshInterval);
 
     return axios.request<ComfortCloudAuthResponse>({
@@ -239,19 +228,15 @@ export default class ComfortCloudApi {
     if (error.response) {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx.
-      this.log.debug(error.response);
-      if (error.response.status === 401) {
-        // Unauthorised, try to log in again
-        this._loginRetryTimeouts.push(setTimeout(this.login.bind(this), LOGIN_RETRY_DELAY));
-      }
+      this.log.error(error.response);
     } else if (error.request) {
       // The request was made but no response was received.
       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
       // http.ClientRequest in node.js
-      this.log.debug(error.request);
+      this.log.error(error.request);
     } else {
       // Something happened in setting up the request that triggered an error.
-      this.log.debug(error.message);
+      this.log.error(error.message);
     }
   }
 }

src/platform.ts
@@ -15,7 +15,12 @@ import IndoorUnitAccessory from './accessories/indoor-unit';
 import OutdoorUnitAccessory from './accessories/outdoor-unit';
 import PanasonicPlatformLogger from './logger';
 import { PanasonicAccessoryContext, PanasonicPlatformConfig } from './types';
-import { LOGIN_RETRY_DELAY, PLATFORM_NAME, PLUGIN_NAME } from './settings';
+import {
+  LOGIN_RETRY_DELAY,
+  MAX_NO_OF_FAILED_LOGIN_ATTEMPTS,
+  PLATFORM_NAME,
+  PLUGIN_NAME,
+} from './settings';
 
 
 /**
@@ -29,7 +34,9 @@ export default class PanasonicPlatform implements DynamicPlatformPlugin {
   // Used to track restored cached accessories
   private readonly accessories: PlatformAccessory<PanasonicAccessoryContext>[] = [];
   private outdoorUnit: OutdoorUnitAccessory | undefined;
+
   private _loginRetryTimeout: NodeJS.Timer | undefined;
+  private noOfFailedLoginAttempts = 0;
 
   public readonly comfortCloud: ComfortCloudApi;
   public readonly log: PanasonicPlatformLogger;
@@ -121,27 +128,35 @@ export default class PanasonicPlatform implements DynamicPlatformPlugin {
     this.comfortCloud.login()
       .then(() => {
         this.log.info('Successfully logged in.');
+        this.noOfFailedLoginAttempts = 0;
         this.configureOutdoorUnit();
         this.discoverDevices();
       })
       .catch(() => {
         this.log.error('Login failed. Skipping device discovery.');
-        this.log.error(
-          'The Comfort Cloud server might be experiencing issues at the moment. ' +
-          `Homebridge will try to log in again in ${LOGIN_RETRY_DELAY / 1000} seconds. ` +
-          'If the issue persists, make sure you configured the correct email and password ' +
-          'and run the latest version of the plugin. ' +
-          'Restart Homebridge when you change your config. ' +
-          'If the error still persists, please report it at ' +
-          'https://github.com/embee8/homebridge-panasonic-ac-platform/issues.',
-        );
+        this.noOfFailedLoginAttempts++;
 
-        // Set an interval to retry this operation.
-        this._loginRetryTimeout = setInterval(this.loginAndDiscoverDevices.bind(this),
-          LOGIN_RETRY_DELAY);
+        if (this.noOfFailedLoginAttempts < MAX_NO_OF_FAILED_LOGIN_ATTEMPTS) {
+          this.log.error(
+            'The Comfort Cloud server might be experiencing issues at the moment. ' +
+            `The plugin will try to log in again in ${LOGIN_RETRY_DELAY / 1000} seconds. ` +
+            'If the issue persists, make sure you configured the correct email and password ' +
+            'and run the latest version of the plugin. ' +
+            'Restart Homebridge when you change your config.',
+          );
+
+          this._loginRetryTimeout = setTimeout(
+            this.loginAndDiscoverDevices.bind(this),
+            LOGIN_RETRY_DELAY,
+          );
+        } else {
+          this.log.error(
+            'Maximum number of failed login attempts reached ' +
+            `(${MAX_NO_OF_FAILED_LOGIN_ATTEMPTS}). ` +
+            'Check your login details and restart Homebridge to reset the plugin.',
+          );
+        }
       });
-
-    this.log.debug(`Finished initialising platform: ${this.platformConfig.name}`);
   }
 
   /**

src/settings.ts
@@ -5,10 +5,12 @@ export const PLUGIN_NAME = 'homebridge-panasonic-ac-platform';
 export const PLATFORM_NAME = 'Panasonic AC Platform';
 
 export const COMFORT_CLOUD_USER_AGENT = 'G-RAC';
-export const APP_VERSION = '1.16.0';
+export const APP_VERSION = '1.17.0';
 
 // 360 sec = 6 min
 export const LOGIN_RETRY_DELAY = 360 * 1000;
+
+export const MAX_NO_OF_FAILED_LOGIN_ATTEMPTS = 5;
 
 // Used to renew the token periodically. Only a safety measure, since we are handling
 // network errors dynamically and re-issuing a login upon a 401 Unauthorized error.

commit 0645ebc10d8772ae2bfe307d96c632a5bf0acc4f

Improve swing mode (#66)
* Implement oscillation functionality (swing modes)

See GitHub issues #27 and #50.

* Remove workflow badge

* Reduce log level of network request error handler

Regression introduced in #58. Issue was pointed out in #63.

.eslintrc
@@ -30,6 +30,7 @@
     "no-multi-spaces": ["warn", { "ignoreEOLComments": false }],
     "no-trailing-spaces": ["warn"],
     "lines-between-class-members": ["warn", "always", {"exceptAfterSingleLine": true}],
+    "operator-linebreak": ["warn", "before", { "overrides": { "?": "before", ":": "before" } }],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",

src/accessories/indoor-unit.ts
@@ -3,7 +3,16 @@ import PanasonicPlatform from '../platform';
 import OutdoorUnitAccessory from './outdoor-unit';
 import { DEVICE_STATUS_REFRESH_INTERVAL } from '../settings';
 import { ComfortCloudDeviceUpdatePayload, PanasonicAccessoryContext } from '../types';
-import { ComfortCloudEcoMode, ComfortCloudFanSpeed } from '../enums';
+import {
+  ComfortCloudAirSwingLR,
+  ComfortCloudAirSwingUD,
+  ComfortCloudEcoMode,
+  ComfortCloudFanAutoMode,
+  ComfortCloudFanSpeed,
+  SwingModeDirection,
+  SwingModePositionLeftRight,
+  SwingModePositionUpDown,
+} from '../enums';
 
 /**
  * An instance of this class is created for each accessory the platform registers.
@@ -288,8 +297,24 @@ export default class IndoorUnitAccessory {
       this.service.getCharacteristic(this.platform.Characteristic.RotationSpeed)
         .updateValue(sliderValue);
 
-      // Swing Mode (optional)
-      if (deviceStatus.airSwingLR === 2 || deviceStatus.airSwingUD === 0) {
+      // Swing Mode
+      if (
+        (
+          deviceStatus.fanAutoMode === ComfortCloudFanAutoMode.AirSwingAuto
+          && this.platform.platformConfig.swingModeDirections
+          === SwingModeDirection.LeftRightAndUpDown
+        )
+        || (
+          deviceStatus.fanAutoMode === ComfortCloudFanAutoMode.AirSwingLR
+          && this.platform.platformConfig.swingModeDirections
+          === SwingModeDirection.LeftRightOnly
+        )
+        || (
+          deviceStatus.fanAutoMode === ComfortCloudFanAutoMode.AirSwingUD
+          && this.platform.platformConfig.swingModeDirections
+          === SwingModeDirection.UpDownOnly
+        )
+      ) {
         this.service.getCharacteristic(this.platform.Characteristic.SwingMode)
           .updateValue(this.platform.Characteristic.SwingMode.SWING_ENABLED);
       } else {
@@ -304,8 +329,8 @@ export default class IndoorUnitAccessory {
       this.service.getCharacteristic(this.platform.Characteristic.CoolingThresholdTemperature)
         .updateValue(setTemperature);
     } catch (error) {
-      this.platform.log.error('An error occurred while refreshing the device status. ' +
-        'Turn on debug mode for more information.');
+      this.platform.log.error('An error occurred while refreshing the device status. '
+        + 'Turn on debug mode for more information.');
 
       // Only log if a Promise rejection reason was provided.
       // Some errors are already logged at source.
@@ -427,12 +452,86 @@ export default class IndoorUnitAccessory {
   async setSwingMode(value: CharacteristicValue) {
     this.platform.log.debug(
       `Accessory: setSwingMode() for device '${this.accessory.displayName}'`);
-    const parameters: ComfortCloudDeviceUpdatePayload = {
-      fanAutoMode: value === this.platform.Characteristic.SwingMode.SWING_ENABLED ? 0 : 1,
-      airSwingLR: 2,
-      airSwingUD: 0,
-    };
+
+    const parameters: ComfortCloudDeviceUpdatePayload = {};
+
+    if (value === this.platform.Characteristic.SwingMode.SWING_ENABLED) {
+      // Activate swing mode
+      // and (if needed) reset one set of fins to their default position.
+      switch (this.platform.platformConfig.swingModeDirections) {
+        case SwingModeDirection.LeftRightAndUpDown:
+          parameters.fanAutoMode = ComfortCloudFanAutoMode.AirSwingAuto;
+          break;
+        case SwingModeDirection.LeftRightOnly:
+          parameters.fanAutoMode = ComfortCloudFanAutoMode.AirSwingLR;
+          parameters.airSwingUD = this.swingModeUpDownToComfortCloudPayloadValue(
+            this.platform.platformConfig.swingModeDefaultPositionUpDown);
+          break;
+        case SwingModeDirection.UpDownOnly:
+          parameters.fanAutoMode = ComfortCloudFanAutoMode.AirSwingUD;
+          parameters.airSwingLR = this.swingModeLeftRightToComfortCloudPayloadValue(
+            this.platform.platformConfig.swingModeDefaultPositionLeftRight);
+          break;
+        default:
+          parameters.fanAutoMode = ComfortCloudFanAutoMode.AirSwingAuto;
+          break;
+      }
+    } else if (value === this.platform.Characteristic.SwingMode.SWING_DISABLED) {
+      // Deactivate swing mode and reset fins to default positions.
+      parameters.fanAutoMode = ComfortCloudFanAutoMode.Disabled;
+      parameters.airSwingLR = this.swingModeLeftRightToComfortCloudPayloadValue(
+        this.platform.platformConfig.swingModeDefaultPositionLeftRight);
+      parameters.airSwingUD = this.swingModeUpDownToComfortCloudPayloadValue(
+        this.platform.platformConfig.swingModeDefaultPositionUpDown);
+    }
     this.sendDeviceUpdate(this.accessory.context.device.deviceGuid, parameters);
+  }
+
+  /**
+   * Maps the internal left-right swing mode position enum to the corresponding
+   * Comfort Cloud value.
+   *
+   * @param position The internal value for the left-right position.
+   * @returns The corresponding Comfort Cloud value for the given position.
+   */
+  swingModeLeftRightToComfortCloudPayloadValue(position?: SwingModePositionLeftRight) {
+    switch (position) {
+      case SwingModePositionLeftRight.Left:
+        return ComfortCloudAirSwingLR.Left;
+      case SwingModePositionLeftRight.CenterLeft:
+        return ComfortCloudAirSwingLR.CenterLeft;
+      case SwingModePositionLeftRight.Center:
+        return ComfortCloudAirSwingLR.Center;
+      case SwingModePositionLeftRight.CenterRight:
+        return ComfortCloudAirSwingLR.CenterRight;
+      case SwingModePositionLeftRight.Right:
+        return ComfortCloudAirSwingLR.Right;
+      default:
+        return ComfortCloudAirSwingLR.Center;
+    }
+  }
+
+  /**
+   * Maps the internal up-down swing mode position enum to the corresponding Comfort Cloud value.
+   *
+   * @param position The internal value for the up-down position.
+   * @returns The corresponding Comfort Cloud value for the given position.
+   */
+  swingModeUpDownToComfortCloudPayloadValue(position?: SwingModePositionUpDown) {
+    switch (position) {
+      case SwingModePositionUpDown.Up:
+        return ComfortCloudAirSwingUD.Up;
+      case SwingModePositionUpDown.CenterUp:
+        return ComfortCloudAirSwingUD.CenterUp;
+      case SwingModePositionUpDown.Center:
+        return ComfortCloudAirSwingUD.Center;
+      case SwingModePositionUpDown.CenterDown:
+        return ComfortCloudAirSwingUD.CenterDown;
+      case SwingModePositionUpDown.Down:
+        return ComfortCloudAirSwingUD.Down;
+      default:
+        return ComfortCloudAirSwingUD.Center;
+    }
   }
 
   async setThresholdTemperature(value: CharacteristicValue) {
@@ -473,8 +572,8 @@ export default class IndoorUnitAccessory {
         this.platform.comfortCloud.setDeviceStatus(guid, payload);
       }
     } catch (error) {
-      this.platform.log.error('An error occurred while sending a device update. ' +
-        'Turn on debug mode for more information.');
+      this.platform.log.error('An error occurred while sending a device update. '
+        + 'Turn on debug mode for more information.');
 
       // Only log if a Promise rejection reason was provided.
       // Some errors are already logged at source.

src/comfort-cloud.ts
@@ -46,8 +46,8 @@ export default class ComfortCloudApi {
         'Content-Type': 'application/json',
         'User-Agent': COMFORT_CLOUD_USER_AGENT,
         'X-APP-TYPE': '0',
-        'X-APP-VERSION': this.config.appVersionOverride ||
-          this.config.latestAppVersion || APP_VERSION,
+        'X-APP-VERSION': this.config.appVersionOverride
+          || this.config.latestAppVersion || APP_VERSION,
       },
       data: {
         'loginId': this.config.email,
@@ -82,8 +82,8 @@ export default class ComfortCloudApi {
     const fetchedDevices: ComfortCloudDevice[] = [];
 
     if (!this.token) {
-      return Promise.reject('No auth token available (login probably failed). ' +
-        'Check your credentials and restart Homebridge.');
+      return Promise.reject('No auth token available (login probably failed). '
+        + 'Check your credentials and restart Homebridge.');
     }
 
     return axios.request<ComfortCloudGroupResponse>({
@@ -94,8 +94,8 @@ export default class ComfortCloudApi {
         'Content-Type': 'application/json',
         'User-Agent': COMFORT_CLOUD_USER_AGENT,
         'X-APP-TYPE': '0',
-        'X-APP-VERSION': this.config.appVersionOverride ||
-          this.config.latestAppVersion || APP_VERSION,
+        'X-APP-VERSION': this.config.appVersionOverride
+          || this.config.latestAppVersion || APP_VERSION,
         'X-User-Authorization': this.token,
       },
     })
@@ -109,8 +109,8 @@ export default class ComfortCloudApi {
           });
         });
         if (fetchedDevices.length === 0) {
-          this.log.info('No devices found. ' +
-            'Check whether you have added at least one device to your Comfort Cloud account.');
+          this.log.info('No devices found. '
+            + 'Check whether you have added at least one device to your Comfort Cloud account.');
         }
         return fetchedDevices;
       })
@@ -131,8 +131,8 @@ export default class ComfortCloudApi {
     this.log.debug(`Comfort Cloud: getDeviceStatus() for device GUID '${deviceGuid}'`);
 
     if (!this.token) {
-      return Promise.reject('No auth token available (login probably failed). ' +
-        'Check your credentials and restart Homebridge.');
+      return Promise.reject('No auth token available (login probably failed). '
+        + 'Check your credentials and restart Homebridge.');
     }
 
     if (!deviceGuid) {
@@ -147,8 +147,8 @@ export default class ComfortCloudApi {
         'Content-Type': 'application/json',
         'User-Agent': COMFORT_CLOUD_USER_AGENT,
         'X-APP-TYPE': '0',
-        'X-APP-VERSION': this.config.appVersionOverride ||
-          this.config.latestAppVersion || APP_VERSION,
+        'X-APP-VERSION': this.config.appVersionOverride
+          || this.config.latestAppVersion || APP_VERSION,
         'X-User-Authorization': this.token,
       },
     })
@@ -176,8 +176,8 @@ export default class ComfortCloudApi {
     this.log.debug(JSON.stringify(parameters, null, 2));
 
     if (!this.token) {
-      return Promise.reject('No auth token available (login probably failed). ' +
-        'Check your credentials and restart Homebridge.');
+      return Promise.reject('No auth token available (login probably failed). '
+        + 'Check your credentials and restart Homebridge.');
     }
 
     if (!deviceGuid) {
@@ -197,8 +197,8 @@ export default class ComfortCloudApi {
         'Content-Type': 'application/json',
         'User-Agent': COMFORT_CLOUD_USER_AGENT,
         'X-APP-TYPE': '0',
-        'X-APP-VERSION': this.config.appVersionOverride ||
-          this.config.latestAppVersion || APP_VERSION,
+        'X-APP-VERSION': this.config.appVersionOverride
+          || this.config.latestAppVersion || APP_VERSION,
         'X-User-Authorization': this.token,
       },
       data: {
@@ -228,15 +228,15 @@ export default class ComfortCloudApi {
     if (error.response) {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx.
-      this.log.error(error.response);
+      this.log.debug(error.response);
     } else if (error.request) {
       // The request was made but no response was received.
       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
       // http.ClientRequest in node.js
-      this.log.error(error.request);
+      this.log.debug(error.request);
     } else {
       // Something happened in setting up the request that triggered an error.
-      this.log.error(error.message);
+      this.log.debug(error.message);
     }
   }
 }

src/enums.ts
@@ -12,3 +12,57 @@ export enum ComfortCloudFanSpeed {
   Four = 4,
   Five = 5,
 }
+
+export enum ComfortCloudFanAutoMode {
+  Disabled = 1,
+  AirSwingAuto = 0,
+  AirSwingLR = 3,
+  AirSwingUD = 2,
+}
+
+export enum ComfortCloudAirSwingUD {
+  Up = 0,
+  CenterUp = 3,
+  Center = 2,
+  CenterDown = 4,
+  Down = 1,
+}
+
+export enum ComfortCloudAirSwingLR {
+  Left = 1,
+  CenterLeft = 5,
+  Center = 2,
+  CenterRight = 4,
+  Right = 0,
+}
+
+// Has to match the 'swingModeDirections'
+// dropdown values in config.schema.json.
+// Otherwise, the plugin setting will be ignored.
+export enum SwingModeDirection {
+  LeftRightAndUpDown = 'LEFT-RIGHT-UP-DOWN',
+  LeftRightOnly = 'LEFT-RIGHT',
+  UpDownOnly = 'UP-DOWN',
+}
+
+// Has to match the 'swingModeDefaultPositionUpDown'
+// dropdown values in config.schema.json.
+// Otherwise, the plugin setting will be ignored.
+export enum SwingModePositionUpDown {
+  Up = 'UP',
+  CenterUp = 'CENTER-UP',
+  Center = 'CENTER',
+  CenterDown = 'CENTER-DOWN',
+  Down = 'DOWN',
+}
+
+// Has to match the 'swingModeDefaultPositionLeftRight'
+// dropdown values in config.schema.json.
+// Otherwise, the plugin setting will be ignored.
+export enum SwingModePositionLeftRight {
+  Left = 'LEFT',
+  CenterLeft = 'CENTER-LEFT',
+  Center = 'CENTER',
+  CenterRight = 'CENTER-RIGHT',
+  Right = 'RIGHT',
+}

src/platform.ts
@@ -101,8 +101,8 @@ export default class PanasonicPlatform implements DynamicPlatformPlugin {
           this.log.info(`The latest app version is ${matches[0]}.`);
           this.platformConfig.latestAppVersion = matches[0];
         } else {
-          this.log.error('Could not find latest app version. ' +
-            'Falling back to override or hard-coded value.');
+          this.log.error('Could not find latest app version. '
+            + 'Falling back to override or hard-coded value.');
         }
       });
     } catch (error) {
@@ -113,14 +113,14 @@ export default class PanasonicPlatform implements DynamicPlatformPlugin {
 
   async loginAndDiscoverDevices() {
     if (!this.platformConfig.email) {
-      this.log.error('Email is not configured - aborting plugin start. ' +
-        'Please set the field `email` in your config and restart Homebridge.');
+      this.log.error('Email is not configured - aborting plugin start. '
+        + 'Please set the field `email` in your config and restart Homebridge.');
       return;
     }
 
     if (!this.platformConfig.password) {
-      this.log.error('Password is not configured - aborting plugin start. ' +
-        'Please set the field `password` in your config and restart Homebridge.');
+      this.log.error('Password is not configured - aborting plugin start. '
+        + 'Please set the field `password` in your config and restart Homebridge.');
       return;
     }
 
@@ -138,11 +138,11 @@ export default class PanasonicPlatform implements DynamicPlatformPlugin {
 
         if (this.noOfFailedLoginAttempts < MAX_NO_OF_FAILED_LOGIN_ATTEMPTS) {
           this.log.error(
-            'The Comfort Cloud server might be experiencing issues at the moment. ' +
-            `The plugin will try to log in again in ${LOGIN_RETRY_DELAY / 1000} seconds. ` +
-            'If the issue persists, make sure you configured the correct email and password ' +
-            'and run the latest version of the plugin. ' +
-            'Restart Homebridge when you change your config.',
+            'The Comfort Cloud server might be experiencing issues at the moment. '
+            + `The plugin will try to log in again in ${LOGIN_RETRY_DELAY / 1000} seconds. `
+            + 'If the issue persists, make sure you configured the correct email and password '
+            + 'and run the latest version of the plugin. '
+            + 'Restart Homebridge when you change your config.',
           );
 
           this._loginRetryTimeout = setTimeout(
@@ -151,9 +151,9 @@ export default class PanasonicPlatform implements DynamicPlatformPlugin {
           );
         } else {
           this.log.error(
-            'Maximum number of failed login attempts reached ' +
-            `(${MAX_NO_OF_FAILED_LOGIN_ATTEMPTS}). ` +
-            'Check your login details and restart Homebridge to reset the plugin.',
+            'Maximum number of failed login attempts reached '
+            + `(${MAX_NO_OF_FAILED_LOGIN_ATTEMPTS}). `
+            + 'Check your login details and restart Homebridge to reset the plugin.',
           );
         }
       });
@@ -197,8 +197,8 @@ export default class PanasonicPlatform implements DynamicPlatformPlugin {
       if (this.platformConfig.exposeOutdoorUnit) {
         if (existingAccessory !== undefined) {
           // The accessory already exists, we only need to set up its handlers.
-          this.log.info(`Restoring accessory '${existingAccessory.displayName}' ` +
-            `(${existingAccessory.UUID}) from cache.`);
+          this.log.info(`Restoring accessory '${existingAccessory.displayName}' `
+            + `(${existingAccessory.UUID}) from cache.`);
           this.outdoorUnit = new OutdoorUnitAccessory(this, existingAccessory);
         } else {
           // The accessory does not yet exist, so we need to create it.
@@ -210,8 +210,8 @@ export default class PanasonicPlatform implements DynamicPlatformPlugin {
       } else {
         if (existingAccessory !== undefined) {
           // This accessory is no longer needed.
-          this.log.info(`Removing accessory '${existingAccessory.displayName}' ` +
-            `(${existingAccessory.UUID}) `);
+          this.log.info(`Removing accessory '${existingAccessory.displayName}' `
+            + `(${existingAccessory.UUID}) `);
           this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
         }
       }
@@ -248,8 +248,8 @@ export default class PanasonicPlatform implements DynamicPlatformPlugin {
 
         if (existingAccessory !== undefined) {
           // The accessory already exists
-          this.log.info(`Restoring accessory '${existingAccessory.displayName}' ` +
-            `(${device.deviceGuid}) from cache.`);
+          this.log.info(`Restoring accessory '${existingAccessory.displayName}' `
+            + `(${device.deviceGuid}) from cache.`);
 
           // If you need to update the accessory.context then you should run
           // `api.updatePlatformAccessories`. eg.:
@@ -288,16 +288,16 @@ export default class PanasonicPlatform implements DynamicPlatformPlugin {
 
           if (comfortCloudDevice === undefined) {
             // This cached devices does not exist on the Comfort Cloud account (anymore).
-            this.log.info(`Removing accessory '${cachedAccessory.displayName}' (${guid}) ` +
-              'because it does not exist on the Comfort Cloud account (anymore?).');
+            this.log.info(`Removing accessory '${cachedAccessory.displayName}' (${guid}) `
+              + 'because it does not exist on the Comfort Cloud account (anymore?).');
 
             this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [cachedAccessory]);
           }
         }
       }
     } catch (error) {
-      this.log.error('An error occurred during device discovery. ' +
-        'Turn on debug mode for more information.');
+      this.log.error('An error occurred during device discovery. '
+        + 'Turn on debug mode for more information.');
       this.log.debug(error);
     }
   }

src/types.ts
@@ -1,4 +1,9 @@
 import { PlatformConfig } from 'homebridge';
+import {
+  SwingModeDirection,
+  SwingModePositionLeftRight,
+  SwingModePositionUpDown,
+} from './enums';
 
 export interface PanasonicPlatformConfig extends PlatformConfig {
   email: string;
@@ -9,6 +14,9 @@ export interface PanasonicPlatformConfig extends PlatformConfig {
   appVersionOverride: string;
   exposeOutdoorUnit: boolean;
   minHeatingTemperature?: number;
+  swingModeDirections?: SwingModeDirection;
+  swingModeDefaultPositionLeftRight?: SwingModePositionLeftRight;
+  swingModeDefaultPositionUpDown?: SwingModePositionUpDown;
 }
 
 export interface PanasonicAccessoryContext {
@@ -103,6 +111,7 @@ export interface ComfortCloudDeviceStatus {
   operationMode: number;
   ecoMode: number;
   fanSpeed: number;
+  fanAutoMode: number;
   airSwingLR: number;
   airSwingUD: number;
   temperatureUnit: string;