c6792e13ed0eafc25cd803b78bfd88607e190052
embee8 <embee8@users.noreply.github.com>
2023-04-17 22:29:28 +0100

    Reduce log level of network request error handler
    Regression introduced in #58. Issue was pointed out in #63.

--- src/comfort-cloud.ts
+++ src/comfort-cloud.ts
@@ -228,15 +228,15 @@ export default class ComfortCloudApi {
     if (error.response) {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx.
-      this.log.error(error.response);
+      this.log.debug(error.response);
     } else if (error.request) {
       // The request was made but no response was received.
       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
       // http.ClientRequest in node.js
-      this.log.error(error.request);
+      this.log.debug(error.request);
     } else {
       // Something happened in setting up the request that triggered an error.
-      this.log.error(error.message);
+      this.log.debug(error.message);
     }
   }
 }

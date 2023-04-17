homebridge-panasonic-ac-platform/homebridge-panasonic-ac-platform

commit 811fc16af2b7e5e3130e2e7eab7cb4ae827cb03a

Improve login error handling (#58)
- Introduce a maximum number of login retry attempts.
- Remove per-device login retries on the back of device status updates.
- Add troubleshooting instructions for login errors (see README).
- Increase app version number.

src/comfort-cloud.ts
@@ -3,7 +3,6 @@ import axios, { AxiosError } from 'axios';
 import {
   APP_VERSION,
   COMFORT_CLOUD_USER_AGENT,
-  LOGIN_RETRY_DELAY,
   LOGIN_TOKEN_REFRESH_INTERVAL,
 } from './settings';
 import {
@@ -22,7 +21,6 @@ import {
 export default class ComfortCloudApi {
   private token: string;
   private _loginRefreshInterval: NodeJS.Timer | undefined;
-  private _loginRetryTimeouts: NodeJS.Timer[] = [];
 
   constructor(
     private readonly config: PanasonicPlatformConfig,
@@ -38,15 +36,6 @@ export default class ComfortCloudApi {
   async login() {
     this.log.debug('Comfort Cloud: login()');
 
-    /**
-     * A repeat-login might have been requested by several accessories
-     * at a similar time. The first timeout to be executed can clear
-     * all remaining ones, since it doesn't make sense to log in multiple
-     * times within a short amount of time.
-     */
-    for (const timeoutId of this._loginRetryTimeouts) {
-      clearTimeout(timeoutId);
-    }
     clearInterval(<NodeJS.Timer>this._loginRefreshInterval);
 
     return axios.request<ComfortCloudAuthResponse>({
@@ -239,19 +228,15 @@ export default class ComfortCloudApi {
     if (error.response) {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx.
-      this.log.debug(error.response);
-      if (error.response.status === 401) {
-        // Unauthorised, try to log in again
-        this._loginRetryTimeouts.push(setTimeout(this.login.bind(this), LOGIN_RETRY_DELAY));
-      }
+      this.log.error(error.response);
     } else if (error.request) {
       // The request was made but no response was received.
       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
       // http.ClientRequest in node.js
-      this.log.debug(error.request);
+      this.log.error(error.request);
     } else {
       // Something happened in setting up the request that triggered an error.
-      this.log.debug(error.message);
+      this.log.error(error.message);
     }
   }
 }

src/platform.ts
@@ -15,7 +15,12 @@ import IndoorUnitAccessory from './accessories/indoor-unit';
 import OutdoorUnitAccessory from './accessories/outdoor-unit';
 import PanasonicPlatformLogger from './logger';
 import { PanasonicAccessoryContext, PanasonicPlatformConfig } from './types';
-import { LOGIN_RETRY_DELAY, PLATFORM_NAME, PLUGIN_NAME } from './settings';
+import {
+  LOGIN_RETRY_DELAY,
+  MAX_NO_OF_FAILED_LOGIN_ATTEMPTS,
+  PLATFORM_NAME,
+  PLUGIN_NAME,
+} from './settings';
 
 
 /**
@@ -29,7 +34,9 @@ export default class PanasonicPlatform implements DynamicPlatformPlugin {
   // Used to track restored cached accessories
   private readonly accessories: PlatformAccessory<PanasonicAccessoryContext>[] = [];
   private outdoorUnit: OutdoorUnitAccessory | undefined;
+
   private _loginRetryTimeout: NodeJS.Timer | undefined;
+  private noOfFailedLoginAttempts = 0;
 
   public readonly comfortCloud: ComfortCloudApi;
   public readonly log: PanasonicPlatformLogger;
@@ -121,27 +128,35 @@ export default class PanasonicPlatform implements DynamicPlatformPlugin {
     this.comfortCloud.login()
       .then(() => {
         this.log.info('Successfully logged in.');
+        this.noOfFailedLoginAttempts = 0;
         this.configureOutdoorUnit();
         this.discoverDevices();
       })
       .catch(() => {
         this.log.error('Login failed. Skipping device discovery.');
-        this.log.error(
-          'The Comfort Cloud server might be experiencing issues at the moment. ' +
-          `Homebridge will try to log in again in ${LOGIN_RETRY_DELAY / 1000} seconds. ` +
-          'If the issue persists, make sure you configured the correct email and password ' +
-          'and run the latest version of the plugin. ' +
-          'Restart Homebridge when you change your config. ' +
-          'If the error still persists, please report it at ' +
-          'https://github.com/embee8/homebridge-panasonic-ac-platform/issues.',
-        );
+        this.noOfFailedLoginAttempts++;
 
-        // Set an interval to retry this operation.
-        this._loginRetryTimeout = setInterval(this.loginAndDiscoverDevices.bind(this),
-          LOGIN_RETRY_DELAY);
+        if (this.noOfFailedLoginAttempts < MAX_NO_OF_FAILED_LOGIN_ATTEMPTS) {
+          this.log.error(
+            'The Comfort Cloud server might be experiencing issues at the moment. ' +
+            `The plugin will try to log in again in ${LOGIN_RETRY_DELAY / 1000} seconds. ` +
+            'If the issue persists, make sure you configured the correct email and password ' +
+            'and run the latest version of the plugin. ' +
+            'Restart Homebridge when you change your config.',
+          );
+
+          this._loginRetryTimeout = setTimeout(
+            this.loginAndDiscoverDevices.bind(this),
+            LOGIN_RETRY_DELAY,
+          );
+        } else {
+          this.log.error(
+            'Maximum number of failed login attempts reached ' +
+            `(${MAX_NO_OF_FAILED_LOGIN_ATTEMPTS}). ` +
+            'Check your login details and restart Homebridge to reset the plugin.',
+          );
+        }
       });
-
-    this.log.debug(`Finished initialising platform: ${this.platformConfig.name}`);
   }
 
   /**

src/settings.ts
@@ -5,10 +5,12 @@ export const PLUGIN_NAME = 'homebridge-panasonic-ac-platform';
 export const PLATFORM_NAME = 'Panasonic AC Platform';
 
 export const COMFORT_CLOUD_USER_AGENT = 'G-RAC';
-export const APP_VERSION = '1.16.0';
+export const APP_VERSION = '1.17.0';
 
 // 360 sec = 6 min
 export const LOGIN_RETRY_DELAY = 360 * 1000;
+
+export const MAX_NO_OF_FAILED_LOGIN_ATTEMPTS = 5;
 
 // Used to renew the token periodically. Only a safety measure, since we are handling
 // network errors dynamically and re-issuing a login upon a 401 Unauthorized error.

commit c6792e13ed0eafc25cd803b78bfd88607e190052

Reduce log level of network request error handler
Regression introduced in #58. Issue was pointed out in #63.

src/comfort-cloud.ts
@@ -228,15 +228,15 @@ export default class ComfortCloudApi {
     if (error.response) {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx.
-      this.log.error(error.response);
+      this.log.debug(error.response);
     } else if (error.request) {
       // The request was made but no response was received.
       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
       // http.ClientRequest in node.js
-      this.log.error(error.request);
+      this.log.debug(error.request);
     } else {
       // Something happened in setting up the request that triggered an error.
-      this.log.error(error.message);
+      this.log.debug(error.message);
     }
   }
 }