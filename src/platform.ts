7343652cfdcb87873bfceb9dd0d67c98d851f03a
mkz212 <82271669+mkz212@users.noreply.github.com>
2023-12-11 18:58:42 +0100

    Update platform.ts

--- src/platform.ts
+++ src/platform.ts
@@ -161,7 +161,9 @@ export default class PanasonicPlatform implements DynamicPlatformPlugin {
           * | 10 | 480 |
           * | ...
           */
-          this.log.error(`Login failed:  ${JSON.stringify(error, null, 2)}`); // error 429 = too many incorect login attempts
+          this.log.error(`Login failed:  ${JSON.stringify(error, null, 2)}`); 
+          // error 429 = too many incorect login attempts
+          
           this.log.error(
             'The Comfort Cloud server might be experiencing issues at the moment. '
             + 'If issue persists, make sure: '
