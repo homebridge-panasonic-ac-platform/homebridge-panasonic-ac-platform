da4487dd3e0f7d41aa7a7de8cafe34e8e6e2833a
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-01-13 13:25:28 +0100

    Update comfort-cloud.ts

--- src/comfort-cloud.ts
+++ src/comfort-cloud.ts
@@ -235,3 +235,54 @@ export default class ComfortCloudApi {
     };
   }
 }
+
+
+function dec2hex(s) {
+  return (s < 15.5 ? '0' : '') + Math.round(s).toString(16);
+}
+
+function hex2dec(s) {
+  return parseInt(s, 16);
+}
+
+function base32tohex(base32) {
+  const base32chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+  const bits = "";
+  const hex = "";
+  
+  for (var i = 0; i < base32.length; i++) {
+    const val = base32chars.indexOf(base32.charAt(i).toUpperCase());
+    bits += leftpad(val.toString(2), 5, '0');
+  }
+  
+  for (var i = 0; i + 4 <= bits.length; i += 4) {
+    const chunk = bits.substr(i, 4);
+    hex = hex + parseInt(chunk, 2).toString(16);
+  }
+  
+  return hex;
+}
+
+function leftpad(str, len, pad) {
+  if (len + 1 >= str.length) {
+    str = Array(len + 1 - str.length).join(pad) + str;
+  }
+  return str;
+}
+
+function generate(secret) {
+
+  const key = base32tohex(secret);
+  const epoch = Math.round(new Date().getTime() / 1000.0);
+  const time = leftpad(dec2hex(Math.floor(epoch / 30)), 16, '0');
+
+  // updated for jsSHA v2.0.0 - http://caligatio.github.io/jsSHA/
+  const shaObj = new jsSHA("SHA-1", "HEX");
+  shaObj.setHMACKey(key, "HEX");
+  const hmac = shaObj.getHMAC("HEX");
+  
+  var otp = (hex2dec(hmac.substr(offset * 2, 8)) & hex2dec('7fffffff')) + '';
+  otp = (otp).substr(otp.length - 6, 6);
+
+  return otp;
+}
