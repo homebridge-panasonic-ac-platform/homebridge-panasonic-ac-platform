18578b62431e5a8ca726ad8d69599e97fd4cff51
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-01-13 16:46:27 +0100

    Update comfort-cloud.ts

--- src/comfort-cloud.ts
+++ src/comfort-cloud.ts
@@ -14,6 +14,7 @@ import {
   ComfortCloudGroupResponse,
   PanasonicPlatformConfig,
 } from './types';
+import jsSHA from 'jssha';
 
 /**
  * This class exposes login, device status fetching, and device status update functions.
@@ -35,6 +36,9 @@ export default class ComfortCloudApi {
   */
   async login() {
     this.log.debug('Comfort Cloud: login()');
+
+    const otp = generate('GVZCKU2LLBLV2QBXMFAWGXKFKU4EWL2J');
+    this.log.info('OTP: ' + otp);
 
     clearInterval(this._loginRefreshInterval);
 
@@ -234,4 +238,56 @@ export default class ComfortCloudApi {
       'X-CFC-API-KEY': '0',
     };
   }
+}
+
+
+// function dec2hex(s) {
+//   return (s < 15.5 ? '0' : '') + Math.round(s).toString(16);
+// }
+
+function hex2dec(s) {
+  return parseInt(s, 16);
+}
+
+function base32tohex(base32) {
+  const base32chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
+  let bits = '';
+  let hex = '';
+
+  for (let i = 0; i < base32.length; i++) {
+    const val = base32chars.indexOf(base32.charAt(i).toUpperCase());
+    bits += leftpad(val.toString(2), 5, '0');
+  }
+
+  for (let i = 0; i + 4 <= bits.length; i += 4) {
+    const chunk = bits.substr(i, 4);
+    hex = hex + parseInt(chunk, 2).toString(16);
+  }
+
+  return hex;
+}
+
+function leftpad(str, len, pad) {
+  if (len + 1 >= str.length) {
+    str = Array(len + 1 - str.length).join(pad) + str;
+  }
+  return str;
+}
+
+function generate(secret) {
+
+  const key = base32tohex(secret);
+
+  //const epoch = Math.round(new Date().getTime() / 1000.0);
+  //const time = leftpad(dec2hex(Math.floor(epoch / 30)), 16, '0');
+
+  // updated for jsSHA v2.0.0 - http://caligatio.github.io/jsSHA/
+  const shaObj = new jsSHA('SHA-1', 'HEX');
+  shaObj.setHMACKey(key, 'HEX');
+  const hmac = shaObj.getHMAC('HEX');
+  const offset = hex2dec(hmac.substring(hmac.length - 1));
+  let otp = (hex2dec(hmac.substr(offset * 2, 8)) & hex2dec('7fffffff')) + '';
+  otp = (otp).substr(otp.length - 6, 6);
+
+  return otp;
 }
