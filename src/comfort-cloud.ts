d898b55589e7466abdea43dbc385b30d1ded9dd0
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-06-09 03:55:37 +0200

    Update comfort-cloud.ts

--- src/comfort-cloud.ts
+++ src/comfort-cloud.ts
@@ -161,7 +161,7 @@ export default class ComfortCloudApi {
       .then((response) => {
         this.log.debug(`Comfort Cloud - getDeviceStatus() for GUID '${deviceGuid}': Success`);
         this.log.debug(response.data);
-        return response.data;
+        return response.data.parameters;
       })
       .catch((error: AxiosError) => {
         this.log.error(`Comfort Cloud - getDeviceStatus() for GUID '${deviceGuid}': Error`);
