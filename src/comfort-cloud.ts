3205cfd56190d64fea33e68d0e7ff247938f850f
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-06-19 21:10:02 +0200

    Update comfort-cloud.ts

--- src/comfort-cloud.ts
+++ src/comfort-cloud.ts
@@ -99,7 +99,7 @@ export default class ComfortCloudApi {
 
     this.log.debug('Comfort Cloud - authorize');
 
-    axios.request({
+    await axios.request({
       method: 'get',
       url: 'https://authglb.digital.panasonic.com/authorize',
       headers: {
@@ -138,7 +138,7 @@ export default class ComfortCloudApi {
 
     this.log.debug('Comfort Cloud - authorize - follow redirect');
 
-    axios.request({
+    await axios.request({
       method: 'get',
       url: 'https://authglb.digital.panasonic.com' + this.location,
       maxRedirects: 0,
@@ -321,11 +321,11 @@ export default class ComfortCloudApi {
 
     // get devices group -------------------------------------
 
-    this.getDevices.bind(this);
+    //this.getDevices.bind(this);
 
     // set timer to refresh token -------------------------------------
 
-    setTimeout(this.refreshToken.bind(this), 86400000);
+    //setTimeout(this.refreshToken.bind(this), 86400000);
 
   }
 
