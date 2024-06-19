ad577d47e463c6645df074e3667abf064718b429
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-06-19 13:20:14 +0200

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -6,6 +6,7 @@ export const PLATFORM_NAME = 'Panasonic AC Platform';
 
 // New API
 // CLIENT_ID and APP_VERSION are related - for each APP VERSION there is a different CLIENT_ID.
+// CLIENT_ID and AUTH0CLIENT are hardcoded in Pansonic Comfort Cloud app(iOS / Android).
 export const APP_VERSION = '1.20.0';
 export const CLIENT_ID = 'Xmy6xIYIitMxngjB2rHvlm6HSDNnaMJx';
 export const AUTH0CLIENT = 'eyJuYW1lIjoiQXV0aDAuQW5kcm9pZCIsImVudiI6eyJhbmRyb2lkIjoiMzAifSwidmVyc2lvbiI6IjIuOS4zIn0=';
