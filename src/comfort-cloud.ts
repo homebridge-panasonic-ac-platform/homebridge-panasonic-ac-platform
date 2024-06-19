e279113940f7fb683ce1417d20622e6aaadb635a
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-06-19 16:54:23 +0200

    Update comfort-cloud.ts

--- src/comfort-cloud.ts
+++ src/comfort-cloud.ts
@@ -207,7 +207,7 @@ export default class ComfortCloudApi {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 '
-          + '(KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36';,
+          + '(KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36',
       },
       data = this.parameters,
       maxRedirects: 0,
