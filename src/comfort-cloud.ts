9a93acafde2f9b133d1d04eda6f1c1c78a161ae8
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-06-20 12:01:59 +0200

    Update comfort-cloud.ts

--- src/comfort-cloud.ts
+++ src/comfort-cloud.ts
@@ -58,6 +58,7 @@ export default class ComfortCloudApi {
     // How to check API: /docs/intercept-traffic.md
 
     // Based on (Python): https://github.com/craibo/panasonic_cc
+    // https://github.com/craibo/panasonic_cc/blob/master/custom_components/panasonic_cc/pcomfortcloud/panasonicsession.py
 
     // Based on (Python): https://github.com/little-quokka/python-panasonic-comfort-cloud/
     // blob/de2869eae5684e5ef5bc8181016902eeb6c73760/pcomfortcloud/panasonic_session.py#L307
