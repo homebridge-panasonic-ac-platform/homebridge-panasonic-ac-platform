457a2d312b3b9eddf8b0c3f600b858565c9d7456
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-07-06 15:44:56 +0200

    Update indoor-unit.ts

--- src/indoor-unit.ts
+++ src/indoor-unit.ts
@@ -864,34 +864,7 @@ export default class IndoorUnitAccessory {
         `${this.accessory.displayName}: ${value === this.platform.Characteristic.Active.ACTIVE ? 'set On' : 'set Off'}`);
     }
 
-    // Workaround - API not storing fanSpeed and ecoMode
-    switch (this.service.getCharacteristic(this.platform.Characteristic.RotationSpeed).value) {
-      case 1:
-        parameters.ecoMode = 2;
-        break;
-      case 2:
-        parameters.fanSpeed = 1;
-        break;
-      case 3:
-        parameters.fanSpeed = 2;
-        break;
-      case 4:
-        parameters.fanSpeed = 3;
-        break;
-      case 5:
-        parameters.fanSpeed = 4;
-        break;
-      case 6:
-        parameters.fanSpeed = 5;
-        break;
-      case 7:
-        parameters.ecoMode = 1;
-        break;
-      default:
-        parameters.ecoMode = 0;
-        parameters.fanSpeed = 0;
-        break;
-    }
+    fanSpeedWorkaround();
 
     this.sendDeviceUpdate(
       this.accessory.context.device.deviceGuid, parameters);
@@ -1261,6 +1234,43 @@ export default class IndoorUnitAccessory {
 
   // ===============================================================================================================================================
 
+  // Workaround - API not storing fanSpeed and ecoMode
+  async fanSpeedWorkaround() {
+
+    this.platform.log.debug(
+      `Accessory: fanSpeedWorkaround() for device '${this.accessory.displayName}'`);
+
+    const parameters: ComfortCloudDeviceUpdatePayload = {};
+
+    switch (this.service.getCharacteristic(this.platform.Characteristic.RotationSpeed).value) {
+      case 1:
+        parameters.ecoMode = 2;
+        break;
+      case 2:
+        parameters.fanSpeed = 1;
+        break;
+      case 3:
+        parameters.fanSpeed = 2;
+        break;
+      case 4:
+        parameters.fanSpeed = 3;
+        break;
+      case 5:
+        parameters.fanSpeed = 4;
+        break;
+      case 6:
+        parameters.fanSpeed = 5;
+        break;
+      case 7:
+        parameters.ecoMode = 1;
+        break;
+      default:
+        parameters.ecoMode = 0;
+        parameters.fanSpeed = 0;
+        break;
+    }
+    this.sendDeviceUpdate(this.accessory.context.device.deviceGuid, parameters);
+  }
 
   async setThresholdTemperature(value: CharacteristicValue) {
     /**
