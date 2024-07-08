46823a70f1e5282fcba239f5de2f4a3bbbc4422f
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-07-08 20:49:33 +0200

    Update comfort-cloud.ts (#437)
    * Update comfort-cloud.ts
    
    * Update comfort-cloud.ts

--- src/comfort-cloud.ts
+++ src/comfort-cloud.ts
@@ -536,7 +536,7 @@ export default class ComfortCloudApi {
     })
       .then((response) => {
         this.log.debug(`${deviceGuid}: Comfort Cloud - getDeviceStatus() : Success`);
-        this.log.debug(`${deviceGuid}: ${JSON.stringify(response.data)}`);
+        this.log.debug(`${deviceGuid}:\n${JSON.stringify(response.data, null, 2)}`);
         return response.data;
       })
       .catch((error: AxiosError) => {
@@ -560,7 +560,7 @@ export default class ComfortCloudApi {
    */
   setDeviceStatus(deviceGuid: string, parameters: ComfortCloudDeviceUpdatePayload) {
     this.log.debug(`${deviceGuid}: Comfort Cloud: setDeviceStatus()`);
-    this.log.debug(`${deviceGuid}: ${JSON.stringify(parameters, null, 2)}`);
+    this.log.debug(`${deviceGuid}: ${JSON.stringify(parameters)}`);
 
     if (!this.token) {
       this.log.error('No auth token available (login probably failed). '
