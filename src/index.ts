a79c2077d821c0c5ffbae31a459784eddb8162c6
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-08-21 17:31:44 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { API } from 'homebridge';
-import PanasonicPlatform from './platform';
-import { PLATFORM_NAME } from './settings';
+import PanasonicPlatform from './platform.js';
+import { PLATFORM_NAME } from './settings.js';
 
 /**
  * This method registers the platform with Homebridge
  */
-export = (api: API) => {
+export default (api: API) => {
   api.registerPlatform(PLATFORM_NAME, PanasonicPlatform);
 };
