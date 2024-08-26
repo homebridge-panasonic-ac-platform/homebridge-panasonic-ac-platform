a9c32056cdba287129f22399150fa79637f8b1b6
mkz212 <82271669+mkz212@users.noreply.github.com>
2024-08-26 22:07:56 +0200

    Update platform.ts

--- src/platform.ts
+++ src/platform.ts
@@ -9,7 +9,7 @@ import {
   Service,
 } from 'homebridge';
 import axios from 'axios';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import ComfortCloudApi from './comfort-cloud';
 import IndoorUnitAccessory from './indoor-unit';
 import PanasonicPlatformLogger from './logger';
